3e96514eb7351bfaa81c0cea46f7f5f6fdf2cc65
Yousuf Khan Ratul <ratulcse27@gmail.com>
2016-05-05 19:08:07 +0600

    comment scraper

--- index.js
+++ index.js
@@ -10,9 +10,15 @@ app.all('/*', function(req, res, next) {
 
 
 
-
+//localhost:3000/app/com.infancyit.fpl
 app.get('/app/:appID', playStore.getAppInfo);
+
+//localhost:3000/keyword/usa
 app.get('/keyword/:word', playStore.getSuggestion);
+
+//localhost:3000/review/com.infancyit.fpl/pageNo
+//localhost:3000/review/com.infancyit.fpl/1
+app.get('/review/:appID/:page?', playStore.getReview);
 
 
 var server = app.listen(3000, function () {
--- routes/store.js
+++ routes/store.js
@@ -26,3 +26,23 @@ exports.getSuggestion = function (req, res) {
             //console.log('There was an error fetching the application!');
         });
 }
+
+exports.getReview = function(req, res){
+    var appID = req.params.appID;
+    var pageID = req.params.page;
+
+    gplay.reviews({
+        appId: appID,
+        page: 0 || pageID,
+        sort: gplay.sort.NEWEST
+    })
+        .then(function(reviews){
+
+            res.json(reviews);
+            console.log('Retrieved application: ' + reviews.length);
+        })
+        .catch(function(e){
+            res.status(400).json({ error: 'Not found.' })
+            //console.log('There was an error fetching the application!');
+        });
+}
